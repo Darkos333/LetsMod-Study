9b14fc9c62f331aa8ed9d893a623b5f3c6d2c1e1
Darkos333 <darkospower@gmail.com>
2016-05-07 16:41:00 +0200

    Fixes versioning

--- src/main/java/com/darkos333/letsmodstudy/reference/Reference.java
+++ src/main/java/com/darkos333/letsmodstudy/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 	public static final String ModID = "LeMoSt";
 	public static final String ModName = "Lets Mod Study";
 	public static final String Fingerprint = "";
-	public static final String Version = "1.7.10-1.0";
+	public static final String Version = "1.7.10-0.1";
 	public static final String ServerProxyClass = "com.darkos333.letsmodstudy.proxy.ServerProxy";
 	public static final String ClientProxyClass = "com.darkos333.letsmodstudy.proxy.ClientProxy";
 	public static final String GuiFactoryClass = "com.darkos333.letsmodstudy.client.gui.GuiFactory";
